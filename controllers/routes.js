c762785c82697dafb28b29f2b99862aef3c2f94f
Owen Kanzler <oj.kanz1@gmail.com>
2024-06-10 21:42:10 -0500

    finished most of the client side javascript

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {Question} = require('../models')
+const { Question } = require("../models");
 
 router.get("/login", (req, res) => {
   // if (req.session.logged_in) {
@@ -10,24 +10,28 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    // THIS IS NOT WORKING. YOU NEED TO RESEAECH THE SEQUELIZE FUNCTIONS. POSSIBLYY RETURN ALL THE DATA AND THEN CHOOSE A RANDON ITEM FROM THE ARRAY RETURNED
-    // const randomQuestion = await Question.find({ order: "RANDOM()" });
-  
-    // const question = randomQuestion.get({plain: true})
-    // console.log(question);
-  
+    const questions = await Question.findAll();
+    if (questions.length === 0) {
+      res.status(404).json({ message: "No questions found!" });
+      return;
+    }
+
+    // Select a random question from the fetched questions
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const randomQuestion = questions[randomIndex].get({ plain: true });
+
     res.render("home", {
-      // question
-    })
+      question: randomQuestion,
+    });
   } catch (error) {
-    res.json(error)
+    res.status(500).json(error);
   }
 });
 
-router.get('/profile/:userId', (req, res) => {
-  res.render('profile')
+router.get("/profile/:userId", (req, res) => {
+  res.render("profile");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 // const helpers = require('./utils/helpers');
 
 const app = express();
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 // Set up sessions with cookies
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {
     // Stored in milliseconds
     maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
@@ -27,14 +27,14 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({  });
+const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
