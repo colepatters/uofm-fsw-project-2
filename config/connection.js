009f05c4346aa9450c99493bf41c0627f12f0a31
anton <antonosland789@gmail.com>
2024-06-17 18:18:44 -0500

    added DB_URL .env

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,33 @@
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize
+
+// sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         host: process.env.DB_URL ?? 'localhost',
+//         dialect: 'postgres'
+//     },
+// );
+
+// module.exports = sequelize;
+
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize
-
-sequelize = new Sequelize(
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_PASSWORD,
     {
-        host: process.env.DB_URL ?? 'localhost',
-        dialect: 'postgres'
-    },
-);
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
 
 module.exports = sequelize;
